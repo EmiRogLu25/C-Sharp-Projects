b4571b1fdf9f9b1584cbbe5b310a65adba188fd2
Yunus Emre KARADAŞ <45971663+EmiRogLu25@users.noreply.github.com>
2020-02-08 21:19:11 +0300

    Add files via upload

---
+++ CapsLockApp/CapsLockApp/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace CapsLockApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 0);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.form_Activated);
+            this.Load += new System.EventHandler(this.form_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ CapsLockApp/CapsLockApp/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace CapsLockApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NotifyIcon capsLockApp = new NotifyIcon();//sistem tepsisindeki uygulama
+        Bitmap icon = new Bitmap(Properties.Resources.capslock);//uygulamanin icon yolu
+
+        //Capslock tusuna uygulama disinda erismek icin gerekli kodlar 
+        [DllImport("user32.dll", CharSet = CharSet.Ansi)]
+        public static extern short GetAsyncKeyState(int Tus);
+
+        public static bool TusKontrol(byte Kod)
+        {
+            if (GetAsyncKeyState((int) Kod) == System.Int16.MinValue + 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void form_Activated(object sender, EventArgs e)
+        {
+            this.Hide();//formu acilista gizler   
+        }
+
+        private void form_Load(object sender, EventArgs e)
+        {
+            capsLockApp.Icon = Icon.FromHandle(icon.GetHicon());//uygulama iconunun belirlenmesi                
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //Eger capslock aktifse icon belirir degilse gizlenir 
+            if (TusKontrol((byte)Keys.CapsLock))
+            {
+                if (Control.IsKeyLocked(Keys.CapsLock))
+                {
+                    capsLockApp.Visible = true;
+                    capsLockApp.Text = "Caps lock Açık.";
+                }
+                else
+                {
+                    capsLockApp.Visible = false;
+                    capsLockApp.Text = "Caps lock Kapalı.";
+                }
+            }
+        }
+    }
+}
